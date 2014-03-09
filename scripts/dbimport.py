d71115713625383e49ddee6894f06dc0d490521d
Harold Cooper <hrldcpr@gmail.com>
2014-03-09 19:51:08 -0400

    insert all info into db

--- scripts/dbimport.py
+++ scripts/dbimport.py
@@ -7,10 +7,13 @@ import psycopg2
 def load(tsv):
     with psycopg2.connect(database='buzzfeed') as db:
         with db.cursor() as cur:
-            cur.execute('create table lists (id serial primary key, n integer, url varchar);')
+            cur.execute('drop table lists;')
+            cur.execute('create table lists (id serial primary key, n integer, url varchar, title varchar, views integer, facebook integer, twitter integer, email integer);')
+            cur.execute('create index on lists (n, views desc);')
 
             for row in csv.DictReader(tsv, delimiter='\t'):
-                cur.execute('insert into lists (n, url) values (%(n)s, %(url)s);',
+                # insert all columns into db:
+                cur.execute('insert into lists ({}) values ({})'.format(','.join(row), ','.join('%({})s'.format(k) if v else 'null' for k, v in row.items())),
                             row)
 
 
