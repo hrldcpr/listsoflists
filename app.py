cd6f776bc9fb2cdb87186aa4921c97a173367a36
Harold Cooper <hrldcpr@gmail.com>
2014-03-09 20:39:50 -0400

    avoid escaping titles, since they are full of &nbsp; and other nonsense

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, Markup
 app = Flask(__name__)
 
 import psycopg2
@@ -26,7 +26,7 @@ def list(n, slug):
                         dict(n=n))
             rows = cur.fetchall()
 
-    items = [Item(*r) for r in rows]
+    items = [Item(Markup(r[0]), r[1]) for r in rows]
     return render_template('list.html', title=slug, items=items)
 
 if __name__ == '__main__':
